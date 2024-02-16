17ab7933e23ed9730fc5c594a6cba46b05652383
alpercubukcu <alpercubukcuu@gmail.com>
2024-02-16 12:04:06 +0300

    delete some using

--- src/Infrastructure/Presistence/Repositories/Repository.cs
+++ src/Infrastructure/Presistence/Repositories/Repository.cs
@@ -4,12 +4,8 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.ChangeTracking;
 using Microsoft.EntityFrameworkCore.Query;
 using Persistence.Context;
-using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Linq.Expressions;
-using System.Text;
-using System.Threading.Tasks;
+
 
 namespace Persistence.Repositories;
 
