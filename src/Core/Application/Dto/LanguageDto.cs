bbb8258e7ea97d64932db039ca2f1c739dd6bc5f
alpercubukcu <alpercubukcuu@gmail.com>
2024-02-19 06:10:32 +0300

    added language dto.

---
+++ src/Core/Application/Dto/LanguageDto.cs
@@ -0,0 +1,21 @@
+﻿
+
+namespace Application.Dto;
+
+public class LanguageDto : BaseDto
+{
+   
+    public string Title { get; set; }
+   
+    public string CountryCode { get; set; }
+    
+    public string Culture { get; set; }
+    
+    public string Flag { get; set; }
+  
+    public int Sort { get; set; }
+  
+    public int? IsRoot { get; set; }
+   
+    public string? Currency { get; set; }
+}
